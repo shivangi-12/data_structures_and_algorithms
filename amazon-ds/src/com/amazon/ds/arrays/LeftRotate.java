1a0a62cfbefb1caef8cada3c44fc01400c598c1a
shivangi-12 <32199332+shivangi-12@users.noreply.github.com>
2020-03-14 14:42:09 +0530

    Rotate Arrays and List changes

---
+++ amazon-ds/src/com/amazon/ds/arrays/LeftRotate.java
@@ -0,0 +1,33 @@
+package com.amazon.ds.arrays;
+
+import java.util.*;
+
+
+public class LeftRotate {
+     static void leftRotate(int arr[],int n,int k){
+       int mod=k%n;
+       for(int i=0;i<n;i++){
+           System.out.print(arr[(i+mod)%n]+" ");
+       }
+     }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+    	int n;
+		int k;
+		int arr[]=new int[20];
+		Scanner in=new Scanner(System.in);
+		System.out.println("Enter the no of elements in the array:");
+		n=in.nextInt();
+		System.out.println("Enter the elements in the array:");
+		for(int i=0;i<n;i++) {
+			arr[i]=in.nextInt();
+		} 
+		System.out.println("Enter the value of k");
+		k=in.nextInt();
+			leftRotate(arr,n,k);	 
+
+    }
+}
